ac61cf479f26180177452f16a37e6f4f18f12066
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-21 12:54:55 +0900

    feat(popup): add language selector

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,12 +1,17 @@
-import { Counter } from '../app/features/counter';
-
 const Popup = () => {
-  document.body.className = 'w-[30rem] h-[15rem]';
+  document.body.className = 'w-[15rem] h-[15rem]';
 
   return (
     <>
-      <div className="flex justify-center mt-2 text-base">Popup Counter</div>
-      <Counter />
+      <div className="flex h-screen items-center justify-center">
+        <p>Select target language: </p>
+        <select name="target_lang">
+          <option value="JA">JA</option>
+          <option value="EN">EN</option>
+          <option value="ZH">ZH</option>
+          <option value="KO">KO</option>
+        </select>
+      </div>
     </>
   );
 };
